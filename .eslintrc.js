d7d298e15b930733af07193318e1f70725608ae2
Jules Forrest <julesforrest@gmail.com>
2019-03-25 18:59:09 -0700

    prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  rules: {
+    "react/jsx-filename-extension": "off"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"]
+};
